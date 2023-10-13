bdbc189410ce11d1f6fcb97f02558571b618809c
Laura Allison Obermaier <lauraallobe@lao21-server.members.linode.com>
2023-10-13 21:25:03 +0000

    added /newsfeed json format

--- hackernews.py
+++ hackernews.py
@@ -1,9 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "<h1>Hacker News home page!</h1>"
+    return render_template('home.html')
+
+
+def fetch_news_item(item_id):
+    item_url = f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
+    response = requests.get(item_url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+@app.route("/newsfeed")
+def newsfeed():
+    api_url = "https://hacker-news.firebaseio.com/v0/newstories.json"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        news_item_ids = response.json()
+
+        news_items = []
+        for item_id in news_item_ids:
+            news_item = fetch_news_item(item_id)
+            if news_item:
+                news_items.append(news_item)
+
+        return jsonify(news_items)
+    else:
+        error_message = f"Failed to fetch newsfeed data. Status code: {response.status_code}"
+        print(error_message)
+        return error_message, 500
 
 if __name__ == '__main__':
     app.run(host='192.155.90.192', debug=True)
