d7bace96b74fa706ba7d6f50bb5cda284bbd1b0e
Laura Allison Obermaier <lauraallobe@lao21-server.members.linode.com>
2023-10-14 18:59:46 +0000

    auth0, need valid domain?

--- hackernews.py
+++ hackernews.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template, request, jsonify, url_for
 import requests
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, render_template, request, jsonify, url_for, redirect, session
+
+#auth0 .env file recognition
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
 app = Flask(__name__)
+#set auth0 secret key
+app.secret_key = env.get("APP_SECRET_KEY")
 
+#configure Authlib to handle application's authentication with Auth0
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+#global store the json news items (currently all from get news items)
 news_items = []
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html', news_items=news_items)
+    return render_template('home.html', news_items=news_items, session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
 
 def fetch_news_item(item_id):
@@ -40,5 +67,35 @@ def newsfeed():
         print(error_message)
         return error_message, 500
 
+#place for auth0
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+#auth0 callback (finished logging in), redirect to home
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+#auth0 logout, clears session & redirect to home
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
 if __name__ == '__main__':
-    app.run(host='192.155.90.192', debug=True)
+    app.run(host='192.155.90.192', port=env.get("PORT", 5000), debug=True)
