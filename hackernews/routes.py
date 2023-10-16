9a5aa4bf942449b3ff41d022623fa50b96a531a3
Laura Allison Obermaier <lauraallobe@lao21-server.members.linode.com>
2023-10-16 02:50:43 +0000

    fixed auth0

--- hackernews/routes.py
+++ hackernews/routes.py
@@ -31,10 +31,9 @@ oauth.register(
 news_items = []
 
 @app.route("/")
-@app.route("/home")
-def hello():
+def home():
     #return render_template('home.html', news_items=news_items, session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
-    return render_template('home.html', news_items=News.query.all(), session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    return render_template("home.html", news_items=News.query.all(), session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
 
 
